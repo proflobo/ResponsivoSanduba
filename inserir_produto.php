be97c1e6d554c7eba8c48785c39253f2dfd52d64
Matheus Lobo <professormatheuslobo@gmail.com>
2024-08-13 12:38:09 -0300

    separando form

--- inserir_produto.php
+++ inserir_produto.php
@@ -1,36 +1,29 @@
 <?php
-/* vamos chamar o arquivo de conexão com o banco para 
-não ter de digitar tudo novamente */
-include('conexao.php');
-//agora vamos inserir nosso produto com un formulario
-    if(isset($_POST["inserir"])){
-        $nome = $_POST["nome"];
-        $tipo = $_POST["tipo"];
-        $descricao = $_POST["descricao"];
-        $valor_unitario = $_POST["valor_unitario"];
-        $quantidade_estoque = $_POST["quantidade_estoque"];
-        $cor = $_POST["cor"];
-        $disponivel = $_POST["disponivel"];
-        $link_imagem = $_POST["link_imagem"];
-        //aqui separado vamos mandar o arquivo(FILE)da imagem
-        $foto = $_FILES['foto'];
-        $nome_foto = $foto['name'];
-        $tmp_nome = $foto['tmp_name'];
-        $destino = 'arquivos/produtos/'.$nome_foto;
-//teste para ver se o arquivo foi**não é necessario
-if(move_uploaded_file($tmp_nome, $destino)){
-    echo "IMAGEM ENVIADO COM SUCESSO!!"
-    }
-    else{
-        echo "ERRO AO ENVIAR A IMAGEM!!";
-    }
-
-    $sql = "INSERT INTO produtos(
-    nome, tipo, descricao,
-     valor_unitario, quantidade_estoque,
-      cor, disponivel, link_imagem, foto)
-      VALUES ($nome,$tipo,$descricao,
-      $valor_unitario, $quantidade_estoque,
-      $cor, $disponivel, '$link_imagem', '$nome_foto')";
+include('conexao.php'); // Conectar ao banco de dados
+if (isset($_POST["inserir"])) {  // Inserir produto
+  $nome = $_POST["nome"];
+  $tipo = $_POST["tipo"];
+  $descricao = $_POST["descricao"];
+  $valor_unitario = $_POST["valor_unitario"];
+  $quantidade_estoque = $_POST["quantidade_estoque"];
+  $cor = $_POST["cor"];
+  $disponivel = $_POST["disponivel"];
+  $link_imagem = $_POST["link_imagem"];
+    $foto = $_FILES['foto']; // Upload da imagem, crie as pastas imagens/produtos/  antes
+    $nome_foto = $foto['name'];
+    $tmp_name = $foto['tmp_name'];
+    $destination = 'imagens/produtos/' . $nome_foto;
+if (move_uploaded_file($tmp_name, $destination)) { // Mover o arquivo para a pasta imagens/produtos
+    echo "Arquivo enviado com sucesso!";
+  } else {
+    echo "Erro ao enviar arquivo!";
+  }
+  // Inserir dados no banco de dados
+  $sql = "INSERT INTO produtos (nome, tipo, descricao, valor_unitario, quantidade_estoque, cor, disponivel, link_imagem, foto) VALUES ('$nome', '$tipo', '$descricao', '$valor_unitario', '$quantidade_estoque', '$cor', '$disponivel', '$link_imagem', '$nome_foto')";
+  if ($conn->query($sql) === TRUE) {
+    echo "Produto inserido com sucesso!";
+  } else {
+    echo "Erro ao inserir produto: " . $conn->error;
+  }
 }
 ?>
